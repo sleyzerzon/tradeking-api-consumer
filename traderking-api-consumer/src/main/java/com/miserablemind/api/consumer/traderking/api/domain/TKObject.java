6ed9f72346ee03dceaf712c548a8b888be3b5fc5
yablinksht <lrimkus@miserablemind.com>
2014-11-10 23:18:32 -0500

    Updated Copyright

--- traderking-api-consumer/src/main/java/com/miserablemind/api/consumer/traderking/api/domain/TKObject.java
+++ traderking-api-consumer/src/main/java/com/miserablemind/api/consumer/traderking/api/domain/TKObject.java
@@ -4,11 +4,6 @@
  * The MIT License (MIT)
  */
 
-/*
- * Miserable Mind
- * http://www.miserablemind.com
- * The MIT License (MIT)
- */
 
 package com.miserablemind.api.consumer.traderking.api.domain;
 
--- traderking-api-consumer/src/main/java/com/miserablemind/api/consumer/traderking/api/impl/OptionQuoteNotFoundException.java
+++ traderking-api-consumer/src/main/java/com/miserablemind/api/consumer/traderking/api/impl/OptionQuoteNotFoundException.java
@@ -4,12 +4,6 @@
  * The MIT License (MIT)
  */
 
-/*
- * Miserable Mind
- * http://www.miserablemind.com
- * The MIT License (MIT)
- */
-
 package com.miserablemind.api.consumer.traderking.api.impl;
 
 public class OptionQuoteNotFoundException extends Exception {
